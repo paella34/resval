8f6b26cfccf2faf0c02961caf1f291114df82242
roiwebco <91880878+NW360D@users.noreply.github.com>
2022-04-11 14:05:22 +0200

    v3
    Test taxes

---
+++ wp-content/plugins/gridzy-builder/js/adm/gzy-adm.js
@@ -0,0 +1,9 @@
+
+// window.onload = function () {
+//     var el = document.getElementById("grid-block_61f2ca3c77f20");
+//     el.onclick = sayHello;
+// }
+
+// function sayHello() {
+//     alert("Hello");
+// }
